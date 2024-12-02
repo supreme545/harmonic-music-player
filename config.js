913f1c3551aa191f8f1c171842f9dbf438d3da04
supreme545 <anuragyad92@gmail.com>
2024-12-03 01:08:09 +0530

    Update config.js with backend URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // Replace this with your deployed backend URL
-    API_BASE_URL: 'https://your-backend-url.onrender.com',
+    API_BASE_URL: 'https://harmonic-music.onrender.com',
     YOUTUBE_API_KEY: '', // Will be used from backend
     ENABLE_YOUTUBE_SEARCH: true,
     ENABLE_NAPSTER_SEARCH: true,
